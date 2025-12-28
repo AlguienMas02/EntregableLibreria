66c720a7342b0af6f9b2fdcb732412cf56922a1d
AlguienMas020 <mpaulinodurand2015@gmail.com>
2025-12-28 17:18:11 -0500

    a

---
+++ EntregableLibreria/Controllers/UsuariosController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EntregableLibreria.Data;
+using EntregableLibreria.Models;
+
+namespace EntregableLibreria.Controllers
+{
+	public class UsuariosController : Controller
+	{
+		private readonly LibreriaContext _context;
+
+		public UsuariosController(LibreriaContext context)
+		{
+			_context = context;
+		}
+
+		// 1. LISTA DE USUARIOS
+		public async Task<IActionResult> Index()
+		{
+			return View(await _context.Usuarios.ToListAsync());
+		}
+
+		// 2. FORMULARIO DE REGISTRO (GET)
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		// 3. GUARDAR USUARIO (POST)
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create(Usuario usuario)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Add(usuario);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			return View(usuario);
+		}
+	}
+}
